4eb7f1190690e1938bf09ab7874609cf8d264784
chris kalkas <chris.kalkas@icloud.com>
2025-03-15 11:07:05 -0500

    Final commit

--- EmailVerification/EmailConformation.cs
+++ EmailVerification/EmailConformation.cs
@@ -13,24 +13,44 @@ namespace Emailverification
        
         public  Boolean VerifyEmail(String Email)
         {
-            Boolean conformation = false;
+            Boolean conformation = true;
+            Boolean AtFound=false;
+            Boolean FirstDot=false;
             Char[] unAcceptedCharacters = { '!', '#', '$', '%', '^', '&', '*', '(', ')','|','\'','/','=','{','}','[',']',':',';','"','\\','<','>','?',',','~','`' };
 
             String _Email = Email;
             for (int i = 0; i < _Email.Length; i++)
             {
+                if (AtFound == true && FirstDot == true)
+                {
+                    if (_Email[i] == '.')
+                    {
+                        conformation = false;
+                    }
+
+                }
+                if (_Email[i] == '@')
+                {
+                    AtFound = true;
+
+                }
+                if (AtFound == true && _Email[i] == '.')
+                {
+                    FirstDot = true;
+
+                }
+               
                 for (int j = 0; j < unAcceptedCharacters.Length; j++)
                 {
-                    if (_Email[i] == ' ' || _Email[0] == '@' || _Email[i] == unAcceptedCharacters[j] || _Email.Length>320 || !_Email.Contains('@') || !_Email.EndsWith(".com") || _Email.)
+                   
+                    if (_Email[i] == ' ' || _Email[0] == '@' || _Email[i] == unAcceptedCharacters[j] || _Email.Length>320 || !_Email.Contains('@') || !_Email.EndsWith(".com"))
                     {
                         conformation = false;
                         goto LoopEnd;
 
                     }
-                    else
-                    {
-                        conformation = true;
-                    }
+                    
+                   
                 }
 
             }
